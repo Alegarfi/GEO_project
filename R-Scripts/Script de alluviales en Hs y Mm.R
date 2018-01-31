e9ecd7e512db771621d2ead36d0343da1b4f1698
Alegarfi <alejandro.garfide@ciencias.unam.mx>
2018-01-31 00:31:48 -0600

    Nuevas gráficas con Hs y Mm

--- R-Scripts/Script de alluviales en Hs y Mm.R
+++ R-Scripts/Script de alluviales en Hs y Mm.R
@@ -6,8 +6,23 @@ Mm <- GEO [grep("Mus_musculus", GEO$ORGANISMO), ]
 homo_musculus<- rbind(Hs, Mm)
 rm(GEO)
 
+
+###LOOP FOR SEPARAR LAS ESPECIES AGLOMERADAS EN UNA MISMA CELDA
+FINAL_DF <-homo_musculus[0, ]
+INTERMEDIATE_DF <- homo_musculus[0, ]
 for(i in 1:nrow(homo_musculus)){
-  FINAL_DF <-homo_musculus[0, ]
-  INTERMEDIATE_DF <- homo_musculus[0, ]
-  message()
+  
+  message(paste0("Procesando la línea ", i))
+  CELDA<-homo_musculus$ORGANISMO[33]
+  ESPECIES<-unlist(strsplit(CELDA, split = ","))
+  ESPECIES <- ESPECIES[!grepl('!Sample', ESPECIES)] 
+  ESPECIES <- ESPECIES [!grepl('1:9', ESPECIES)]
+  NUMERO_DE_ESPECIES <- length(ESPECIES)
+  for(x in 1:NUMERO_DE_ESPECIES){
+    INTERMEDIATE_DF[x,] <-homo_musculus[i,]
+    INTERMEDIATE_DF[x,"ORGANISMO"] <- ESPECIES[x]
+  }
+  FINAL_DF<-rbind(FINAL_DF, INTERMEDIATE_DF)
 }
+
+rm(INTERMEDIATE_DF)
